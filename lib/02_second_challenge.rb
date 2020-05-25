e528ffa461e4b8c754856f99e1048156e4dd82f7
Lauren Frankovich <frankovister@gmail.com>
2020-05-25 14:03:49 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -2,53 +2,15 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {},
-      :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
-      },
-      :hero => {
-        :name => "Romeo",
-        :age => "15"
-      },
-      :hero_friends => [
-        {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
-        },
-        {
-          :name => "Mercutio",
-          :age => "18",
-          :attitude => "hot-headed"
-        }
-        ]
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
     },
     :capulet => {
-      :patriarch => {
-        :name => "Lord Capulet",
-        :age => "50"
-      },
-      :matriarch => {
-        :name => "Lady Capulet",
-        :age => "51"
-      },
-      :heroine => {
-        :name => "Juliet",
-        :age => "15",
-        :status => "alive"
-      },
-      :heroine_friends => [
-        {
-          :name => "Steven",
-          :age => "30",
-          :attitude => "confused"
-        },
-        {
-          :name => "Nurse",
-          :age => "44",
-          :attitude => "worried"
-        }
-      ]
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
     }
   }
 end
